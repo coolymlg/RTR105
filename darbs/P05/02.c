4aa0e3db0763fb620d39217a4809c1985d469e7a
user <danik.best0401@gmail.com>
2019-09-26 15:40:15 +0300

    2019-09-26-15-40

--- darbs/P05/02.c
+++ darbs/P05/02.c
@@ -2,13 +2,13 @@
 #include <limits.h>
 int main ()
 {
-	int a = 50000; //50 ,000
-	int b = 1000000; //1 ,000 ,000
-	int c = a*b; //kaads ir sagaidams rezultaats?
+	long long int a = 50000; //50 ,000
+	long long int b = 1000000; //1 ,000 ,000
+	long long int c = a*b; //kaads ir sagaidams rezultaats?
 
-printf ("int datu tipa izmers ir %d baiti \n",sizeof (int) );
+printf ("int datu tipa izmers ir %ld baiti \n",sizeof (int) );
 printf (" Apreekjinajam a un b reizinajumu : \n" );
-printf ("a = %d, b= %d \n", a, b);
-printf ("c = a*b= %d*%d = %d \n", a,b,c );// rrezultaats uz ekrana
+printf ("a = %lld, b= %lld \n", a, b);
+printf ("c = a*b= %lld*%lld = %lld \n", a,b,c );// rrezultaats uz ekrana
 }
 
